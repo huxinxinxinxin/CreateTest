82c67059bc17b8950ec4cdb7017a2ed66f9c8ff6
huxin <345598716@qq.com>
2017-06-29 18:09:27 +0800

    fix code, 窗口不正常显示问题，优化文档

--- resources/test.sh
+++ resources/test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/expect
+trap {
+ set rows [stty rows]
+ set cols [stty columns]
+ stty rows $rows columns $cols < $spawn_out(slave,name)
+} WINCH
 set username [lindex $argv 0]
 set hostname [lindex $argv 1]
 set password [lindex $argv 2]
